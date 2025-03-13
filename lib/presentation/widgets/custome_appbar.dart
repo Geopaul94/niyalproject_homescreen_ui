e1adf6438986a5f01aa97c10be67b3051e734705
geopaul94 <geopaul94@gmail.com>
2025-03-13 14:06:23 +0530

    first commit

---
+++ lib/presentation/widgets/custome_appbar.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CustomeAppbar extends StatelessWidget {
+  const CustomeAppbar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(children: [],);
+  }
+}
